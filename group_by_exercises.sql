91743f6374be81111cd9a817c87c45d068f68fd2
davidandstephens <stephens.a.david@gmail.com>
2018-12-03 13:52:15 -0600

    finished the group by exercise

---
+++ group_by_exercises.sql
@@ -0,0 +1,9 @@
+USE employees;
+
+SELECT DISTINCT title FROM titles;
+
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%e' GROUP BY first_name, last_name;
+
+SELECT last_name, COUNT(last_name) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name ORDER BY COUNT(last_name) DESC;
+
+SELECT COUNT(*), gender FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') GROUP BY gender ORDER BY COUNT(*) DESC;
