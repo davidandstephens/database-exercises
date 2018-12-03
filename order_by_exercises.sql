5265995b1c1031af99af13101f26c80847863e91
davidandstephens <stephens.a.david@gmail.com>
2018-12-03 11:39:26 -0600

    made the change in where to order by

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -4,4 +4,4 @@ USE employees;
 
 # SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
 
-# SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%12-25%' ORDER BY birth_date ASC, hire_date DESC;
+# SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%12-25' ORDER BY birth_date ASC, hire_date DESC;
